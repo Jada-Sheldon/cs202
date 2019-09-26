ce2129049ed85de01f9b0163f1d3d4246b1b22cc
Jada Sheldon <jmsheldon2@yahoo.com>
2019-09-26 13:12:19 -0800

    created the the empty funtions in the class Box

--- HW01/box.cpp
+++ HW01/box.cpp
@@ -1,1 +1,30 @@
+// Jada Sheldon
+
 #include "box.hpp"
+#include <iostream>
+#include <string>
+using std::string;
+
+void Box::setWidth() {
+
+}
+
+void Box::setHeight() {
+
+}
+
+void Box::getWidth() {
+
+}
+
+void Box::getHeight() {
+
+}
+
+string Box::filled() {
+
+}
+
+void Box::print(ostream&) {
+
+}
--- HW01/box.hpp
+++ HW01/box.hpp
@@ -1,12 +1,24 @@
+// Jada Sheldon
+
 #pragma once
 #ifndef BOX_HPP
 #define BOX_HPP
 
+#include <iostream>
+#include <sstream>
+using std::ostream;
+
 class Box {
 public:
-	
+	void setWidth();
+	void setHeight();
+	void getWidth();
+	void getHeight();
+	string filled();
+	void print(ostream&);
 
 private:
+	bool full;
 
 };
 
