5dfb24edc05b4872c9932ca9fa2b1b0c9fc7c9bc
Jada Sheldon <jmsheldon2@yahoo.com>
2019-10-03 10:57:37 -0800

    fixed the first constructor

--- Lab8/complex.cpp
+++ Lab8/complex.cpp
@@ -1,6 +1,6 @@
 #include "complex.hpp"
 
-Complex::Complex(double real): _real(real){
+Complex::Complex(double real): _real(real), _imag(0){
 }
 
 Complex::Complex(double real, double image) : _real(real), _imag(image) {
