24602e51be7da1599db499ae5ce464b8b1740278
Jada Sheldon <jmsheldon2@yahoo.com>
2019-10-03 11:12:13 -0800

    added the os operator

--- Lab8/complex.cpp
+++ Lab8/complex.cpp
@@ -1,7 +1,21 @@
 #include "complex.hpp"
+#include <iostream>
 
 Complex::Complex(double real): _real(real), _imag(0){
 }
 
 Complex::Complex(double real, double image) : _real(real), _imag(image) {
+}
+
+double Complex::getReal() const{
+	return _real;
+}
+
+double Complex::getImag() const{
+	return _imag;
+}
+
+std::ostream & operator<<(std::ostream & os, const Complex & num) {
+	os << num.getReal();
+	return os;
 }
--- Lab8/complex.hpp
+++ Lab8/complex.hpp
@@ -2,14 +2,19 @@
 #ifndef COMPLEX_HPP
 #define COMPLEX_HPP
 
+#include <ostream>
+
 class Complex {
 public:
 	Complex(double real);
 	Complex(double real, double image);
-
+	double getReal() const;
+	double getImag() const;
 private:
 	double _real;
 	double _imag;
 };
+
+std::ostream& operator<<(std::ostream&, const Complex&);
 
 #endif // !COMPLEX_HPP
