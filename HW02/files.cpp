a41b979dd04e994f18dddf90c3888c5f121861d1
Jada Sheldon <jmsheldon2@yahoo.com>
2019-10-05 12:18:45 -0800

    attempted to read id

--- HW02/files.cpp
+++ HW02/files.cpp
@@ -6,9 +6,14 @@ using std::string;
 #include <fstream>
 using std::ofstream;
 using std::ifstream;
+#include <sstream>
+using std::istringstream;
 
 int getIdFromFile(const string & fileName, istream & is, ostream & os) {
 	ofstream ifile(fileName);
+	string line;
+	getline(is, line);
+	istringstream istring(line);
 	while (true) {
 		if (!ifile) {
 			if (ifile.eof()) {
@@ -20,7 +25,11 @@ int getIdFromFile(const string & fileName, istream & is, ostream & os) {
 			}
 		}
 		else {
+			int id;
+			istring >> id;
+			os << id << "\n";
 			//read file
+			
 		}
 	}
 }
