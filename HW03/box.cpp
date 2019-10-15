ce695f50d57cc4a5e22eb2cdb6fd7243b3f5d224
Jada Sheldon <jmsheldon2@yahoo.com>
2019-10-14 19:37:00 -0800

    created HW03 and copied and pasted old box.cpp and box.hpp

---
+++ HW03/box.cpp
@@ -0,0 +1,77 @@
+// Jada Sheldon
+
+#include "box.hpp"
+#include <iostream>
+#include <string>
+using std::string;
+#include <sstream>
+using std::ostringstream;
+
+int Box::setWidth(int widthSize) {
+	_width = widthSize;
+	return _width;
+}
+
+int Box::setHeight(int heightSize) {
+	_height = heightSize;
+	return _height;
+}
+
+int Box::getWidth() const {
+	return _width;
+}
+
+int Box::getHeight() const {
+	return _height;
+}
+
+string Box::type() const {
+	if (full == true) {
+		return "Filled";
+	}
+	else {
+		return "Hollow";
+	}
+}
+
+void Box::print(ostream& os) const {
+	for (int i = 0; i < _height; i++) {
+		if (full == true) {
+			for (int j = 0; j < _width; j++) {
+				os << "x";
+			}
+			os << "\n";
+		}
+		else {
+			if (i == 0 || i == _height - 1) {
+				for (int j = 0; j < _width; j++) {
+					os << "x";
+				}
+			}
+			else {
+				for (int j = 0; j < _width; j++) {
+					if (j == 0 || j == _width - 1) {
+						os << "x";
+					}
+					else {
+						os << " ";
+					}
+				}
+			}
+			os << "\n";
+
+		}
+	}
+}
+
+Box::Box() : _width(1), _height(1), full(true) {
+
+}
+
+Box::Box(int userWidth, int userHeight) : _width(userWidth), _height(userHeight), full(true) {
+
+}
+
+Box::Box(int userWidth, int userHeight, bool userType) : _width(userWidth), _height(userHeight), full(userType) {
+
+}
---
+++ HW03/box.hpp
@@ -0,0 +1,31 @@
+//Jada Sheldon
+
+#pragma once
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include <iostream>
+#include <sstream>
+using std::ostream;
+#include <string>
+using std::string;
+
+class Box {
+public:
+	int setWidth(int widthSize);
+	int setHeight(int heightSize);
+	int getWidth() const;
+	int getHeight() const;
+	string type() const;
+	void print(ostream& os) const;
+	Box();
+	Box(int userWidth, int userHeight);
+	Box(int userWidth, int userHeight, bool userType);
+
+private:
+	bool full;
+	int _width;
+	int _height;
+};
+
+#endif
