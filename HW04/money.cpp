0778de3396b601b8da4c1b7bf3049beabdc8ec96
Jada Sheldon <jmsheldon2@yahoo.com>
2019-10-23 20:27:20 -0800

    created the constuctures and two over loaded operators

--- HW04/money.cpp
+++ HW04/money.cpp
@@ -1,1 +1,35 @@
 #include "money.hpp"
+
+void Money::setCents(int cents) {
+	_cents = cents;
+}
+
+int Money::getCents() const{
+	return _cents;
+}
+
+Money::Money(): _cents(0)
+{
+}
+
+Money::Money(int dollars, int cents):_cents(dollars*100 + cents)
+{
+}
+
+Money::Money(double dollerCents): _cents(dollerCents*100+0.5)
+{
+}
+
+Money& Money::operator+=(const Money& rhs)
+{
+	_cents += rhs.getCents();
+}
+
+Money operator+(const Money& lhs, const Money& rhs)
+{
+	Money temp{ lhs };
+	temp += rhs;
+	return temp;
+}
+
+
--- HW04/money.hpp
+++ HW04/money.hpp
@@ -4,13 +4,17 @@
 
 class Money {
 public:
-	
-
+	void setCents(int cents);
+	int getCents() const;
+	Money();
+	Money(int dollars, int cents);
+	Money(double dollerCents);
+	Money& operator+=(const Money &rhs);
 
 private:
-
+	int _cents;
 };
 
-
+Money operator+(const Money& lhs, const Money& rhs);
 
 #endif
