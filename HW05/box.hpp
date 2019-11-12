bede6dd3e9c098f9145519b59eb838a9f80ff3c5
Jada Sheldon <jmsheldon2@yahoo.com>
2019-11-11 19:40:53 -0900

    created HW05 and created empty classes for the boxes

---
+++ HW05/box.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+class Box {
+public:
+
+private:
+
+};
+
+class FilledBox {
+public:
+
+private:
+
+};
+
+class HollowBox {
+public:
+
+private:
+
+};
+
+class CheckeredBox {
+public:
+
+private:
+
+};
