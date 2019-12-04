018513d66f11a7c13a4d8f2d45aee65a4989fe68
Jada Sheldon <jmsheldon2@yahoo.com>
2019-12-03 15:42:57 -0900

    Fixed the the operators * and -> and also fixed the printVector function. Then I added the pointer delete to the destructor

--- HW06/printVectorAndRAIIptr.hpp
+++ HW06/printVectorAndRAIIptr.hpp
@@ -6,24 +6,68 @@
 #include <iostream>
 
 template <typename pvector>
-std::ostream printVector(std::ostream& os, const std::vector<pvector>& vectortype) {
+std::ostream& printVector(std::ostream& os, const std::vector<pvector>& vectortype) {
 	for (auto i:vectortype) {
-		os << vectortype[i] << "\n";
+		os << i << "\n";
 	}
 	return os;
 }
 
+
+
+
+
+
+
 template <typename ObjectType>
 class RAIIPtr {
+	/*template <typename U>
+	friend ObjectType& operator* (RAIIPtr<ObjectType>& object);*/
 public:
+	/*ObjectType& operator* () {
+		return *_ptr;
+	}*/
+	ObjectType& operator*();
+	/*ObjectType* operator->() {
+		return _ptr;
+	}*/
+	ObjectType* operator->();
 	RAIIPtr(ObjectType* object) : _ptr(object) {
 	}
-	~RAIIPtr{
-		//delete pointer
+	~RAIIPtr(){
+		delete _ptr;
 	}
 private:
 	ObjectType* _ptr;
 };
 
 
+
+
+//template <typename ObjectType>
+//ObjectType& operator*(RAIIPtr<ObjectType>& object) {
+//	return object._ptr;
+//}
+
+template<typename ObjectType>
+inline ObjectType& RAIIPtr<ObjectType>::operator*()
+{
+	return *_ptr;
+}
+
+template<typename ObjectType>
+inline ObjectType* RAIIPtr<ObjectType>::operator->()
+{
+	return _ptr;
+}
+
+//template<typename ObjectType>
+//ObjectType& RAIIPtr<ObjectType>::operator->(const RAIIPtr<ObjectType>& ptr)
+//{
+//	return ptr;
+//}
+
+
 #endif // !PRINTVECTORANDRAIIPTR_HPP
+
+
